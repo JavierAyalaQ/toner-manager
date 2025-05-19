1f11b9052b5c5ff10115436b692b2bf057a3a8d5
Javier Ayala <javierayala1618@gmail.com>
2025-05-19 07:57:32 -0500

    midlwr

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { defineMiddleware } from "astro/middleware";
 export const onRequest = defineMiddleware(({ cookies, redirect, url }, next) => {
   const isLoggedIn = cookies.get("auth")?.value === "true";
 
-  const protectedRoutes = ["/manage", "/logs", "/"];
+  const protectedRoutes = ["/manage", "/logs"];
 
   const isProtectedRoute = protectedRoutes.some((route) => url.pathname.startsWith(route));
 
