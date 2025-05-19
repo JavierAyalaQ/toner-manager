0da1dd2057bf9b78ba88f2a45f359efa1ecc13d5
Javier Ayala <javierayala1618@gmail.com>
2025-05-19 11:18:57 -0500

    Added neon db

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,11 @@
+interface ImportMetaEnv {
+  readonly NEON_DATABASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 import type { HTMLTag, Polymorphic } from "astro/types";
 
 type ActionProps<Tag extends HTMLTag> = Polymorphic<{
