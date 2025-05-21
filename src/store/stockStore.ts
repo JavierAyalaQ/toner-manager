25b924686ffb8aa941217db9df08fe78a61c7732
Javier Ayala <javierayala1618@gmail.com>
2025-05-21 15:57:48 -0500

    pending update to socket io

--- src/store/stockStore.ts
+++ src/store/stockStore.ts
@@ -2,26 +2,25 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 export type Stock = {
-  EPSON: number;
-  CANON: number;
-  EPSON2: number;
-  CANON2: number;
+  id: number;
+  name: string;
+  quantity: number;
 }
 
 export interface StockStore {
-  stock: Stock;
+  stock: Stock[];
   updateStock: (item: string, quantity: number) => void;
 }
 
 export const useStockStore = create<StockStore>()(
   persist(
     (set) => ({
-      stock: {
-        EPSON: 0,
-        CANON: 0,
-        EPSON2: 0,
-        CANON2: 0,
-      },
+      stock: [
+        { id: 1, name: "CANON", quantity: 0 },
+        { id: 2, name: "HP", quantity: 0 },
+        { id: 3, name: "EPSON", quantity: 0 },
+        { id: 4, name: "CANON2", quantity: 0 },
+      ],
       updateStock: (item, quantity) => set((state) => ({
         stock: { ...state.stock, [item]: quantity },
       })),
@@ -32,15 +31,5 @@ export const useStockStore = create<StockStore>()(
   )
 );
 
-/* 
-import io from "socket.io-client";
-const socket = io("http://localhost:3000");
 
-type Stock = { id: number; name: string; quantity: number };
-
-type StockStore = {
-  stock: Stock[];
-  updateStock: (stock: Stock) => void;
-};
-
-*/
+//pending
